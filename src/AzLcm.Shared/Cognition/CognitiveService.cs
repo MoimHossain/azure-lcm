36b77760fe6f798f3ccb3ab02d41524fad7c9655
Moim HA <moimhossain@gmail.com>
2025-01-28 07:49:27 +0100

    Update Azure.Identity and Azure.AI.OpenAI package versions across projects

--- src/AzLcm.Shared/Cognition/CognitiveService.cs
+++ src/AzLcm.Shared/Cognition/CognitiveService.cs
@@ -6,29 +6,26 @@ using AzLcm.Shared.Storage;
 using Azure;
 using Azure.AI.OpenAI;
 using Azure.Core;
-using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.AI;
 using Microsoft.Extensions.Logging;
 using System.ServiceModel.Syndication;
 using System.Text;
 using System.Text.Json;
+using ChatMessage = Microsoft.Extensions.AI.ChatMessage;
 
 namespace AzLcm.Shared.Cognition
 {
     public class CognitiveService(
-        JsonSerializerOptions jsonSerializerOptions,
         AzureCredentialProvider azureCredentialProvider,
         ILogger<CognitiveService> logger,
         DaemonConfig daemonConfig)
     {
-        private ChatCompletionsOptions GetChatCompletionsOptions(float temperature = (float)1) => new()
-        {
-            DeploymentName = daemonConfig.AzureOpenAIGPTDeploymentId,
-            ChoiceCount = 1,
-            MaxTokens = 4000,
+        private static ChatOptions GetChatOptions() => new()
+        {            
             FrequencyPenalty = (float)0,
             PresencePenalty = (float)0,
-            Temperature = (float)temperature
+            Temperature = (float)1
         };
 
         public async Task<Verdict?> AnalyzeV2Async(
@@ -36,28 +33,27 @@ namespace AzLcm.Shared.Cognition
         {
             ArgumentNullException.ThrowIfNull(nameof(feedItem));
 
-            var openAIClient = await GetOpenAIClient(stoppingToken);
-
-            var thread = GetChatCompletionsOptions((float)0.7);
-
-            thread.Messages.Add(new ChatRequestSystemMessage(promptTemplate));
-
+            List<ChatMessage> messages = [new(ChatRole.System, promptTemplate)];
             var tags = feedItem.Categories.Select(c => c.Name).ToList();
-
             var feedDetails = new StringBuilder();
             feedDetails.AppendLine($"<Update info BEGIN>");
             feedDetails.AppendLine($"Title: {feedItem.Title.Text}");
             feedDetails.AppendLine($"Summary: {feedItem.Summary.Text}");
             feedDetails.AppendLine($"Tags: {string.Join(", ", tags)}");
-            feedDetails.AppendLine($"</Update info END>");
-            thread.Messages.Add(new ChatRequestUserMessage(feedDetails.ToString()));
+            feedDetails.AppendLine($"</Update info END>");            
+            messages.Add(new ChatMessage(ChatRole.User, feedDetails.ToString()));
 
             try
             {
-                var response = await openAIClient.GetChatCompletionsAsync(thread, stoppingToken);
-                var rawContent = response.Value.Choices[0].Message.Content;
-                var verdict = Verdict.FromJson(rawContent, logger, jsonSerializerOptions);
-                return verdict;
+                var openAIClient = await GetOpenAIClient(stoppingToken);
+                var chatClient = openAIClient.AsChatClient(modelId: daemonConfig.AzureOpenAIGPTDeploymentId);
+
+                var response = await chatClient
+                    .CompleteAsync<Verdict>(messages, GetChatOptions(), cancellationToken: stoppingToken);
+                if (response.TryGetResult(out var orchestrationGroundedResponse) && orchestrationGroundedResponse != null)
+                {
+                    return orchestrationGroundedResponse;
+                }
             }
             catch (Exception ex)
             {
@@ -72,14 +68,14 @@ namespace AzLcm.Shared.Cognition
         {
             ArgumentNullException.ThrowIfNull(nameof(areaPathServiceMapConfig));
 
-            var openAIClient = await GetOpenAIClient(stoppingToken);
+            var validAreaPaths = areaPathServiceMapConfig.Map.Select(m => m.RouteToAreaPath).ToList();
+            validAreaPaths.Add(areaPathServiceMapConfig.DefaultAreaPath);
 
-            var thread = GetChatCompletionsOptions((float)1);
-            thread.Messages.Add(new ChatRequestSystemMessage(
-"""
+            List<ChatMessage> messages = [new(ChatRole.System, 
+"""                
 You have a map of services to area paths. Your task is to map the service name to the area path.
-"""));
-
+""" 
+            )];
             var mapInfoBuilder = new StringBuilder();
             foreach (var mapItem in areaPathServiceMapConfig.Map)
             {
@@ -88,57 +84,33 @@ You have a map of services to area paths. Your task is to map the service name t
                 mapInfoBuilder.AppendLine($" AreaPath: {mapItem.RouteToAreaPath}");
                 mapInfoBuilder.AppendLine($"] {Environment.NewLine}");
             }
-            thread.Messages.Add(new ChatRequestSystemMessage(mapInfoBuilder.ToString()));
+            messages.Add(new ChatMessage(ChatRole.System, mapInfoBuilder.ToString()));
+            messages.Add(new ChatMessage(ChatRole.User, $"Service Name: {serviceName}"));
 
-            thread.Messages.Add(new ChatRequestSystemMessage(
-"""
-IMPORTANT: You MUST response with JSON object with following schema:
-
-export type MapResponse {
-    areaPath: string;
-}
-
-Example1: If user provide a service name "Azure SQL" and it matches to an area path '/area-path/demo'
-Response should be:
-```
-    { areaPath: "/area-path/demo", matched: true }
-```
-
-Example2: If user provide a service name "Azure X-Service" and there is no match found
-Response should be:
-```
-    { areaPath: "", matched: false }
-```
-"""));
-            thread.Messages.Add(new ChatRequestSystemMessage("The response MUST contain ONLY valid JSON object like examples, no additional explanation or text!!"));
-
-            thread.Messages.Add(new ChatRequestUserMessage($"Service Name: {serviceName}"));
-
-            var rawContent = string.Empty;
+            
             try
             {
-                var response = await openAIClient.GetChatCompletionsAsync(thread, stoppingToken);
-                rawContent = response.Value.Choices[0].Message.Content;
-                var mapResponse = AreaPathMapResponse.FromJson(rawContent, jsonSerializerOptions);
+                var openAIClient = await GetOpenAIClient(stoppingToken);
+                var chatClient = openAIClient.AsChatClient(modelId: daemonConfig.AzureOpenAIGPTDeploymentId);
 
-                var validAreaPaths = areaPathServiceMapConfig.Map.Select(m => m.RouteToAreaPath).ToList();
-                validAreaPaths.Add(areaPathServiceMapConfig.DefaultAreaPath);
+                var response = await chatClient
+                    .CompleteAsync<AreaPathMapResponse>(messages, GetChatOptions(), cancellationToken: stoppingToken);
 
-                if (mapResponse != null)
+                if (response.TryGetResult(out var orchestrationGroundedResponse) && orchestrationGroundedResponse != null)
                 {
-                    if (!validAreaPaths.Contains($"{mapResponse.AreaPath}"))
+                    if (!validAreaPaths.Contains($"{orchestrationGroundedResponse.AreaPath}"))
                     {
                         return new AreaPathMapResponse { AreaPath = string.Empty };
                     }
                     else
                     {
-                        return mapResponse;
+                        return orchestrationGroundedResponse;
                     }
                 }
             }
             catch (Exception ex)
             {
-                logger.LogError(ex, message: $"Raw: {rawContent}");
+                logger.LogError(ex, message: $"Error in MapServiceToAreaPathAsync()");
             }
             return null;
         }
@@ -147,15 +119,15 @@ Response should be:
         public async Task<bool> CheckOpenAIAccessAsync(CancellationToken stoppingToken)
         {
             var openAIClient = await GetOpenAIClient(stoppingToken);
+            var chatClient = openAIClient.AsChatClient(modelId: daemonConfig.AzureOpenAIGPTDeploymentId);
 
-            var thread = GetChatCompletionsOptions((float)1);
-            thread.Messages.Add(new ChatRequestSystemMessage("""Reply with one word only."""));
-            thread.Messages.Add(new ChatRequestUserMessage($"How are you?"));
+            List<ChatMessage> messages = [new(ChatRole.System, """Reply with one word only.""")];
+            messages.Add(new ChatMessage(ChatRole.User, $"How are you?"));
 
             try
             {
-                var response = await openAIClient.GetChatCompletionsAsync(thread, stoppingToken);
-                logger.LogInformation("OpenAI Access Check Response: {Response}", response.Value.Choices[0].Message.Content);
+                var response = await chatClient.CompleteAsync(messages, GetChatOptions(), cancellationToken: stoppingToken);
+                logger.LogInformation("OpenAI Access Check Response: {Response}", response.Message.Text);
                 return true;
             }
             catch (Exception ex)
@@ -166,7 +138,7 @@ Response should be:
         }
 
 
-        public async Task<OpenAIClient> GetOpenAIClient(CancellationToken cancellationToken)
+        public async Task<AzureOpenAIClient> GetOpenAIClient(CancellationToken cancellationToken)
         {
             if (_openAIClient == null)
             {
@@ -176,7 +148,7 @@ Response should be:
                 {
                     throw new InvalidOperationException("Failed to read the OpenAI config from KeyVault");
                 }
-                _openAIClient = new OpenAIClient(openAIEndpoint, new AzureKeyCredential(openAIKey));
+                _openAIClient = new AzureOpenAIClient(openAIEndpoint, new AzureKeyCredential(openAIKey));
             }
             return _openAIClient;
         }
@@ -199,8 +171,8 @@ Response should be:
                         }
                     };
                     var kvClient = new SecretClient(new Uri(daemonConfig.KeyVaultURI), azureCredentialProvider.GetKVCredential(), options);
-                    KeyVaultSecret openAIEndpoint = await kvClient.GetSecretAsync("AOIEndpoint");
-                    KeyVaultSecret openAiKey = await kvClient.GetSecretAsync("AOIKey");
+                    KeyVaultSecret openAIEndpoint = await kvClient.GetSecretAsync("AOIEndpoint", cancellationToken: cancellationToken);
+                    KeyVaultSecret openAiKey = await kvClient.GetSecretAsync("AOIKey", cancellationToken: cancellationToken);
                     if (openAIEndpoint != null)
                     {
                         logger.LogInformation("OpenAI URI: {OpenAIEndpoint}", openAIEndpoint.Value);
@@ -215,7 +187,7 @@ Response should be:
             return new(new Uri("https://microsoft.com"), string.Empty, false);
         }
 
-        private OpenAIClient? _openAIClient = null;
+        private AzureOpenAIClient? _openAIClient = null;
     }
 }
 
